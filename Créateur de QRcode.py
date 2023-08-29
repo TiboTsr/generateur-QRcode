748cca80829d5cd0ebb55144a76b59e3f9d8a7d2
Tibotsr <67163860+TiboTsr@users.noreply.github.com>
2023-08-29 10:33:18 +0200

    Créateur de QRcode V.5.py
    5 eme version du créateur de QRcode

--- Créateur de QRcode.py
+++ Créateur de QRcode.py
@@ -1,5 +1,6 @@
 import qrcode
 from PIL import Image
+import os
 
 colors = ["aliceblue", "antiquewhite", "aqua", "aquamarine", "azure", "beige", "bisque", "black", "blanchedalmond", "blue", "blueviolet", "brown", "burlywood", "cadetblue", "chartreuse", "chocolate", "coral", "cornflowerblue", "cornsilk", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgray", "darkgreen", "darkkhaki", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "rebeccapurple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellow", "yellowgreen"]
 
@@ -60,9 +61,14 @@ if color2 == color1 :
 else : 
     img=qr.make_image(fill_color=color1,back_color=color2)
 
-img.save('C:/Users/TTESSIER/Downloads/qrcode.png')
+download_dir = os.path.expanduser('~') + '/Downloads/'
+
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
+
+img.save(os.path.join(download_dir, 'qrcode.png'))
 
 print("Votre QRcode vers", data, "a bien été enregistré dans le dossier téléchargement")
 
-image = Image.open('C:/Users/TTESSIER/Downloads/qrcode.png')
+image = Image.open((os.path.join(download_dir, 'qrcode.png')))
 image.show()
